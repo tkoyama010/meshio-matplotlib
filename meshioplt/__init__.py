123e2fcdf37fac491844cf15a85b929193b702d0
Tetsuo Koyama <tkoyama010@gmail.com>
2020-04-19 14:15:18 +0900

    :wrench: C0301: Line too long

--- meshioplt/__init__.py
+++ meshioplt/__init__.py
@@ -1,7 +1,7 @@
 """
-[meshio](https://github.com/nschloe/meshio):heart: [matplotlib](https://github.com/matplotlib/matplotlib)
+meshio :heart: matplotlib
 
-:exclamation: Inspired by [Custom 3D engine in Matplotlib](https://matplotlib.org/matplotblog/posts/custom-3d-engine/)
+:exclamation: Inspired by Custom 3D engine in Matplotlib
 """
 
 class Mesh:
