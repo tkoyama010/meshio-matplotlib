1e1a2bda3085066f63918638e0424819fcaa8d33
Tetsuo Koyama <tkoyama010@gmail.com>
2020-04-19 04:16:49 +0000

    :test_tube: mesh object

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,23 @@
-import meshio
 import matplotlib.pyplot as plt
+import meshio
+import numpy as np
+
 import meshioplt as mplt
 
+
 def test():
+    points = np.array(
+        [
+            [0.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [1.0, 1.0, 0.0],
+            [0.0, 0.0, 1.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0],
+        ]
+    )
+    cells = [("hexahedron", np.array([[0, 1, 3, 2, 4, 5, 7, 6]]))]
+    mesh = mplt.mesh(points, cells)
     return True
