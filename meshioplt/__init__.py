8099fbfc9645a8594c2f594d4f8039bfe7768fdd
Tetsuo Koyama <tkoyama010@gmail.com>
2020-04-19 14:29:00 +0000

    :new: method hexahedrons_to_quads

--- meshioplt/__init__.py
+++ meshioplt/__init__.py
@@ -47,27 +47,44 @@ class Mesh:
         """
         faces = []
         for cell in self.cells:
+            cell_type = cell[0]
             cell_datas = cell[1]
-            face_type = "quad"
-            face_datas = []
-            for cell_data in cell_datas:
-                face_datas.append(
-                    [cell_data[0], cell_data[1], cell_data[2], cell_data[3]]
-                )
-                face_datas.append(
-                    [cell_data[5], cell_data[4], cell_data[7], cell_data[6]]
-                )
-                face_datas.append(
-                    [cell_data[4], cell_data[0], cell_data[3], cell_data[7]]
-                )
-                face_datas.append(
-                    [cell_data[1], cell_data[5], cell_data[6], cell_data[2]]
-                )
-                face_datas.append(
-                    [cell_data[4], cell_data[5], cell_data[1], cell_data[0]]
-                )
-                face_datas.append(
-                    [cell_data[3], cell_data[2], cell_data[6], cell_data[7]]
-                )
-            faces.append((face_type, np.array(face_datas)))
+            if cell_type == "hexahedron":
+                face_type = "quad"
+                face_datas = self.hexahedrons_to_quads(cell_datas)
+                faces.append((face_type, np.array(face_datas)))
         return faces
+
+    def hexahedrons_to_quads(self, cell_datas: np.ndarray) -> np.ndarray:
+        """hexahedrons_to_quads
+
+         convert hexahedrons cells to quad faces.
+
+        Args:
+           cell_datas: point index of cells
+
+        Returns:
+           face_datas: point index of faces
+
+        """
+        face_datas = []
+        for cell_data in cell_datas:
+            face_datas.append(
+                [cell_data[0], cell_data[1], cell_data[2], cell_data[3]]
+            )
+            face_datas.append(
+                [cell_data[5], cell_data[4], cell_data[7], cell_data[6]]
+            )
+            face_datas.append(
+                [cell_data[4], cell_data[0], cell_data[3], cell_data[7]]
+            )
+            face_datas.append(
+                [cell_data[1], cell_data[5], cell_data[6], cell_data[2]]
+            )
+            face_datas.append(
+                [cell_data[4], cell_data[5], cell_data[1], cell_data[0]]
+            )
+            face_datas.append(
+                [cell_data[3], cell_data[2], cell_data[6], cell_data[7]]
+            )
+        return face_datas
