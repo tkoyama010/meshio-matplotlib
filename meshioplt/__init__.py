cfdb603159927971ca0f7a8a375ed6acb7a466d1
Tetsuo Koyama <tkoyama010@gmail.com>
2020-04-19 05:41:14 +0000

    :wrench: C0115: Missing class docstring

--- meshioplt/__init__.py
+++ meshioplt/__init__.py
@@ -1,10 +1,36 @@
-"""
-meshio :heart: matplotlib
+"""meshio :heart: matplotlib
 
-:exclamation: Inspired by Custom 3D engine in Matplotlib
+     * :exclamation: Inspired by Custom 3D engine in Matplotlib
 """
+
 
 class Mesh:
+    """Mesh object
+
+     Mesh information of plot
+
+    Attributes:
+        points : points of mesh.
+        cells : cells of mesh.
+
+    """
+
     def __init__(self, points, cells):
-        self.points = points
-        self.cells = cells
+        self._points = points
+        self._cells = cells
+
+    @property
+    def points(self):
+        """points
+
+         points of mesh.
+        """
+        return self._points
+
+    @property
+    def cells(self):
+        """cells
+
+         cells of mesh.
+        """
+        return self._cells
