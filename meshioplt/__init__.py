9828d5f1a983d6a0ced1abfb946e2b3c5a7276c7
Tetsuo Koyama <tkoyama010@gmail.com>
2020-04-19 14:13:00 +0900

    :wrench: C0114: Missing module docstring

--- meshioplt/__init__.py
+++ meshioplt/__init__.py
@@ -1,3 +1,9 @@
+"""
+[meshio](https://github.com/nschloe/meshio):heart: [matplotlib](https://github.com/matplotlib/matplotlib)
+
+:exclamation: Inspired by [Custom 3D engine in Matplotlib](https://matplotlib.org/matplotblog/posts/custom-3d-engine/)
+"""
+
 class Mesh:
     def __init__(self, points, cells):
         self.points = points
