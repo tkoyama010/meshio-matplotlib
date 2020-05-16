a3bda2d6e08c4636207ae06ebf226dd95e63b059
Tetsuo Koyama <tkoyama010@gmail.com>
2020-05-16 22:44:04 +0900

    :globe_with_meridians: locale_dirs

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+locale_dirs = [os.path.join(basedir, "../locale/")]
 
 
 # -- Project information -----------------------------------------------------
